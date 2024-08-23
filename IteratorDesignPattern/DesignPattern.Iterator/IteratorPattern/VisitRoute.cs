98d5ff21f1244ebab15986a1d8cf2435f93e5a4f
Seçil Seleci <143125124+secilseleci@users.noreply.github.com>
2024-08-23 14:45:25 +0300

    iterator design pattern eklendi.

---
+++ IteratorDesignPattern/DesignPattern.Iterator/IteratorPattern/VisitRoute.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Iterator.IteratorPattern
+{
+    public class VisitRoute
+    {
+        public string CountryName { get; set; }
+        public string CityName { get; set; }
+        public string VisitPlaceName { get; set; }
+
+    }
+}
